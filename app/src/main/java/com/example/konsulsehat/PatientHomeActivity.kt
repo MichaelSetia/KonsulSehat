a9813f4fa3f0afd34bf0e92f996ac3aace2b5d7d
Michael <76718092+MichaelSetia@users.noreply.github.com>
2024-05-28 15:19:58 +0700

    Update Home Fragment

--- app/src/main/java/com/example/konsulsehat/PatientHomeActivity.kt
+++ app/src/main/java/com/example/konsulsehat/PatientHomeActivity.kt
@@ -13,39 +13,39 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 
-class PatientHomeActivity : AppCompatActivity() {
-    lateinit var binding: ActivityPatientHomeBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private var cloudDB = Firebase.firestore
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityPatientHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        binding.btnLogout.setOnClickListener{
-            signOutAndStartSignInActivity()
-        }
-    }
-
-    private fun signOutAndStartSignInActivity() {
-        auth.signOut()
-
-        googleSignInClient.signOut().addOnCompleteListener(this) {
-            // Optional: Update UI or show a message to the user
-            val intent = Intent(this@PatientHomeActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-    }
-}
+//class PatientHomeActivity : AppCompatActivity() {
+//    lateinit var binding: ActivityPatientHomeBinding
+//    private lateinit var auth: FirebaseAuth
+//    private lateinit var googleSignInClient: GoogleSignInClient
+//    private var cloudDB = Firebase.firestore
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityPatientHomeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        auth = FirebaseAuth.getInstance()
+//        val currentUser = auth.currentUser
+//
+//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+//            .requestEmail()
+//            .build()
+//        googleSignInClient = GoogleSignIn.getClient(this, gso)
+//
+//        binding.btnLogout.setOnClickListener{
+//            signOutAndStartSignInActivity()
+//        }
+//    }
+//
+//    private fun signOutAndStartSignInActivity() {
+//        auth.signOut()
+//
+//        googleSignInClient.signOut().addOnCompleteListener(this) {
+//            // Optional: Update UI or show a message to the user
+//            val intent = Intent(this@PatientHomeActivity, LoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+//    }
+//}
