236a41b2b6361666720375d846b4464e79555468
Carla <crystalitacarla@gmail.com>
2024-06-10 20:21:31 +0700

    Bisa ngechat + nampilin
    tapi tampilan bubble chatnya masih kacau gr2 orderBy e error gatau opoo

--- app/src/main/java/com/example/konsulsehat/ChatAdapter.kt
+++ app/src/main/java/com/example/konsulsehat/ChatAdapter.kt
@@ -1,22 +1,31 @@
 package com.example.konsulsehat
 
+import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.coroutineContext
 
 class ChatAdapter(
     var chatList: List<Map<String, Any>>,
     var userLoggedIn: String,
-
+    clickListener: ClickListener
     ): RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
+
+    private var clickListener:ClickListener = clickListener
 
     class ViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
         val tvNama : TextView = row.findViewById(R.id.tvNamaDokter)
@@ -24,6 +33,7 @@ class ChatAdapter(
         val tvImg: ImageView = row.findViewById(R.id.imgProfileDoctor)
 
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatAdapter.ViewHolder {
         val layout = LayoutInflater.from(parent.context).inflate(
             R.layout.layout_chat, parent, false
@@ -34,20 +44,41 @@ class ChatAdapter(
 
     override fun onBindViewHolder(holder: ChatAdapter.ViewHolder, position: Int) {
         val data = chatList[position]
-        if (data["sender"].toString() == userLoggedIn.toString()) {
-            holder.tvNama.text = data["receiver"].toString()
-            holder.tvLastChat.text = data["sender"].toString()
-        }else if (data["receiver"].toString() == userLoggedIn.toString()) {
-            holder.tvNama.text = data["sender"].toString()
-            holder.tvLastChat.text = data["receiver"].toString()
+        if (data["user_1"].toString() == userLoggedIn.toString()) {
+            holder.tvNama.text = data["user_2_name"].toString()
+            val profileUrl = data["profile_pict_user_2"].toString()
+//            Glide.with(ChatFragment())
+//                .load(profileUrl)
+//                .placeholder(R.drawable.icon_profile) // optional placeholder
+//                .error(R.drawable.icon_profile) // optional error image
+//                .into(holder.tvImg)
+            holder.tvLastChat.text = data["last_chat"].toString()
+        }else if (data["user_2"].toString() == userLoggedIn.toString()) {
+            holder.tvNama.text = data["user_1_name"].toString()
+            val profileUrl = data["profile_pict_user_1"].toString()
+//            Glide.with(ChatFragment())
+//                .load(profileUrl)
+//                .placeholder(R.drawable.icon_profile) // optional placeholder
+//                .error(R.drawable.icon_profile) // optional error image
+//                .into(holder.tvImg)
+            holder.tvLastChat.text = data["last_chat"].toString()
         }
 
 
-//        holder.tvImg.setImageResource(th["profile_pict"]as Int)
+        holder.itemView.setOnClickListener {
+            val roomId = data["documentId"] as? String // Replace "roomId" with the actual field name in your data
+            if (roomId != null) {
+                clickListener.clickedItem(roomId)
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
         return chatList.size
     }
 
+    interface ClickListener{
+        fun clickedItem(roomId : String)
+    }
 }
---
+++ app/src/main/java/com/example/konsulsehat/ChatRoomActivity.kt
@@ -0,0 +1,169 @@
+package com.example.konsulsehat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ChatRoomActivity : AppCompatActivity() {
+    var chatList = mutableListOf<Map<String, Any>>()
+    lateinit var rvRoomChat: RecyclerView
+    lateinit var chatRoomAdapter: ChatRoomAdapter
+    lateinit var tvUsernameChat : TextView
+    private lateinit var userLoggedIn : String
+    private lateinit var tempReceiver : String
+    private lateinit var tempReceiverName : String
+    private lateinit var tempSender : String
+    private lateinit var tempSenderName : String
+    private lateinit var roomChatId : String
+    private lateinit var inpIsiChat: EditText
+    private lateinit var btnSendChat: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+
+        inpIsiChat = findViewById(R.id.inpIsiChat)
+        btnSendChat = findViewById(R.id.btnSendChat)
+        tvUsernameChat = findViewById(R.id.tvUsernameChat)
+        rvRoomChat = findViewById(R.id.rvRoomChat)
+
+        userLoggedIn = ""
+
+        rvRoomChat.layoutManager = LinearLayoutManager(this)
+
+        getData()
+
+        btnSendChat.setOnClickListener {
+            sendMessage()
+        }
+    }
+
+    private fun getData() {
+        var intent = intent.extras
+
+        roomChatId = intent!!.getString("roomId").toString()
+        userLoggedIn = intent!!.getString("userLoggedIn").toString()
+//        Toast.makeText(this, roomChatId, Toast.LENGTH_SHORT).show()
+        fetchChatDataFromFirestore()
+    }
+
+    private fun fetchChatDataFromFirestore() {
+        var doctorName : String = ""
+        val db = FirebaseFirestore.getInstance()
+        db.collection("chats")
+            .whereEqualTo("fk_roomchat".toString(), roomChatId)
+//            .orderBy("timestamp")
+            .get()
+            .addOnSuccessListener { result ->
+                chatList.clear()
+                for (document in result) {
+                    val chatData = document.data
+                    val receiver = chatData["receiver"] as? String
+                    val sender = chatData["sender"] as? String
+                    val receiver_name = chatData["receiver_name"] as? String
+                    val sender_name = chatData["sender_name"] as? String
+
+                    chatList.add(chatData)
+
+                    if (receiver == userLoggedIn) {
+                        if (doctorName=="") {
+                            doctorName = sender_name.toString()
+                        }
+                    }else if (sender == userLoggedIn) {
+                        if (doctorName=="") {
+                            doctorName = receiver_name.toString()
+                        }
+                    }
+                }
+                setDoctorName(doctorName)
+                chatRoomAdapter = ChatRoomAdapter(chatList, userLoggedIn)
+                rvRoomChat.adapter = chatRoomAdapter
+            }
+            .addOnFailureListener { exception ->
+                Log.w("FirestoreData", "Error getting documents: ", exception)
+            }
+    }
+
+    fun setDoctorName(name: String) {
+        var doctorName = name
+        // Update the doctor's name if the view is already created
+        tvUsernameChat.text = doctorName
+    }
+
+    private fun sendMessage() {
+        val message = inpIsiChat.text.toString()
+
+        if (message.isNotEmpty()) {
+            val db = FirebaseFirestore.getInstance()
+
+            //set receiver and sender
+            db.collection("room_chat")
+                .document(roomChatId)
+                .get()
+                .addOnSuccessListener {document ->
+                    if (document != null && document.exists()) {
+                        val roomChatData = document.data
+                        if (roomChatData?.get("user_1").toString() == userLoggedIn) {
+                            tempSender = roomChatData?.get("user_1").toString()
+                            tempSenderName = roomChatData?.get("user_1_name").toString()
+                            tempReceiver = roomChatData?.get("user_2").toString()
+                            tempReceiverName = roomChatData?.get("user_2_name").toString()
+                        } else {
+                            tempSender = roomChatData?.get("user_2").toString()
+                            tempSenderName = roomChatData?.get("user_2_name").toString()
+                            tempReceiver = roomChatData?.get("user_1").toString()
+                            tempReceiverName = roomChatData?.get("user_1_name").toString()
+                        }
+                        val chatData = hashMapOf(
+                            "sender" to userLoggedIn,
+                            "sender_name" to tempSenderName,
+                            "receiver" to tempReceiver, // Replace with actual receiver ID
+                            "receiver_name" to tempReceiverName,
+                            "isi_chat" to message,
+                            "timestamp" to System.currentTimeMillis(),
+                            "fk_roomchat" to roomChatId
+                        )
+                        db.collection("chats")
+                            .add(chatData)
+                            .addOnSuccessListener { documentReference ->
+                                chatData["id"] = documentReference.id // Optional: store the document ID if needed
+                                chatList.add(chatData)
+                                chatRoomAdapter.notifyItemInserted(chatList.size - 1)
+                                rvRoomChat.scrollToPosition(chatList.size - 1)
+                                inpIsiChat.text.clear() // Clear the input field
+
+                                db.collection("room_chat")
+                                    .document(roomChatId)
+                                    .update(
+                                        mapOf(
+                                            "last_chat" to message,
+                                        )
+                                    )
+                                    .addOnSuccessListener {
+                                        Log.d("FirestoreData", "Room chat last message updated successfully")
+                                    }
+
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w("FirestoreData", "Error adding document", e)
+                            }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("FirestoreData", "Error getting documents: ", exception)
+                }
+
+        }
+    }
+
+}
