4bfda3c2dcde9cf9b2401fc0be8fb2b14331c121
Carla <crystalitacarla@gmail.com>
2024-06-20 15:11:30 +0700

    Tampilan Detail Doctor + Patient
    (belum backend + passing)

---
+++ app/src/main/java/com/example/konsulsehat/Admin/DetailMasterPatientActivity.kt
@@ -0,0 +1,12 @@
+package com.example.konsulsehat.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.konsulsehat.R
+
+class DetailMasterPatientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_master_patient)
+    }
+}
