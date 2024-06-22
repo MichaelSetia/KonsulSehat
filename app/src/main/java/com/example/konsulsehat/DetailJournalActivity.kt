5c587649e18dd6be3b4fa2875932990e76f6dfe6
Carla <crystalitacarla@gmail.com>
2024-06-22 19:41:49 +0700

    Tampilan Journal
    Nav Journal, Layout Journal, Add Journal, Detail Journal

---
+++ app/src/main/java/com/example/konsulsehat/DetailJournalActivity.kt
@@ -0,0 +1,11 @@
+package com.example.konsulsehat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailJournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_journal)
+    }
+}
